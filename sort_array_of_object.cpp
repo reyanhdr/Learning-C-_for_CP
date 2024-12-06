7374ba689868250629e99fb5ca5a25cdf7cad824
Abu Bakkar Siddik <156587892+reyanhdr@users.noreply.github.com>
2024-12-06 22:02:56 +0600

    Add files via upload

---
+++ sort_array_of_object.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+    public:
+    string name ;
+    int roll;
+    int marks;
+    
+};
+
+// bool cmp(Student l, Student r)
+// {
+//     // if (l.roll < r.roll){  // assending order (<) // dessending order (>)
+//     //     return true;
+//     // }
+//     // else {
+//     //     return false;
+//     // }
+
+//     return l.marks > r.marks; // shortcut uporer statement
+
+// }
+
+bool cmp (Student l, Student r)
+{
+    if (l.marks > r.marks){
+        return true;
+    }
+    else if (l.marks < r.marks){
+        return false;
+    }
+    else 
+    {
+        if (l.roll < r.roll)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    Student a[n];
+
+    for (int i = 0 ; i < n; i++){
+
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+
+   sort (a,a+n,cmp);
+   for (int i =0 ; i < n; i++){
+
+       cout << a[i].name << " " << a[i].roll << " " << a[i].marks <<  endl;
+   }
+
+   
+   
+
+
+    return 0;
+}
